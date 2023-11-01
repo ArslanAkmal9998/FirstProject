61dc24c7820dfe4d0fbcf274981c9cd2a5745944
ArslanAkmal9998 <arslan.akmal@gametrain.org|>
2023-11-01 11:17:01 +0500

    First Commit

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+
+const dataProducts=async()=>{
+    const data=await fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>{return json})
+
+    data.forEach(element => {
+const heading =document.createElement('h2');
+const image = document.createElement('img');
+heading.innerHTML=element.title;
+image.src=element.image;
+image.classList.add("image")
+
+document.body.appendChild(heading)
+document.body.appendChild(image)
+
+
+    });
+}
